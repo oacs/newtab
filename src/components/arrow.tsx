6aa0e5dee397bf61416fd895c93e8b3fb618384a
Oscar Castillejo <oacs1221@gmail.com>
2023-04-30 14:45:20 -0400

    feat: add svg arrow component

---
+++ src/components/arrow.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+type ArrowUpIconProps = {
+  fill: string;
+  rotate: number;
+};
+
+const ArrowUpIcon = ({ fill, rotate }: ArrowUpIconProps) => {
+  return (
+    <div style={{ transform: `rotate(${rotate}deg)` }}>
+      <svg
+        height="24"
+        viewBox="0 0 24 24"
+        fill={fill}
+        stroke={fill}
+        strokeWidth={5}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="feather feather-arrow-up"
+      >
+        <line x1="12" y1="19" x2="12" y2="5"></line>
+        <polyline points="5 12 12 5 19 12"></polyline>
+      </svg>
+    </div>
+  );
+};
+
+export default ArrowUpIcon;
